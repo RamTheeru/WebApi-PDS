b5c2d65d78037402210337ce5da4dbe1ed3f5777
ram theertha <theertha808@gmail.com>
2020-11-01 14:44:08 +0530

    IIS expresss issue resolved

--- pdstest/Models/UserType.cs
+++ pdstest/Models/UserType.cs
@@ -9,14 +9,7 @@ namespace pdstest.Models
     {
         public int UserTypeId { get; set; }
         public string User { get; set; }
-        public string Role { get { return Role; } set { if (UserTypeId == 1) Role = "Admin";
-                else if (UserTypeId == 2) Role = "HRLE";
-                else if (UserTypeId == 3) Role = "HRHE";
-                else if (UserTypeId == 4) Role = "FLE";
-                else if (UserTypeId == 5) Role = "FHE";
-                else if (UserTypeId == 6) Role = "ELE";
-                else if (UserTypeId == 7) Role = "EHE";
-            } }
+        public string Role { get; set; }
 
         public bool Valid { get; set; }
     }
