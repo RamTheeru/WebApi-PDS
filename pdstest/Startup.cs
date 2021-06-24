17e6f779b703d92378353a9681eb0f9ee0102d5a
ram theertha <theertha808@gmail.com>
2021-06-24 17:19:06 +0530

    test swagger doc

--- pdstest/Startup.cs
+++ pdstest/Startup.cs
@@ -181,7 +181,7 @@ namespace pdstest
             //});
                 //app.UseHttpsRedirection();
             app.UseSwagger();
-            app.UseSwaggerUI(sw => sw.SwaggerEndpoint("./v1/swagger.json", "API for PDS"));
+            app.UseSwaggerUI(sw => sw.SwaggerEndpoint("../v1/swagger.json", "API for PDS"));
             //app.UseMvc();
            
    
