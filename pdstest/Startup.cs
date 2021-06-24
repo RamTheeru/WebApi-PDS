33b42f090f24610757f3a50fc6f4e145a7170fdd
ram theertha <theertha808@gmail.com>
2021-06-24 18:13:10 +0530

    test urls for swagger

--- pdstest/Startup.cs
+++ pdstest/Startup.cs
@@ -181,7 +181,9 @@ namespace pdstest
             //});
                 //app.UseHttpsRedirection();
             app.UseSwagger();
-            app.UseSwaggerUI(sw => sw.SwaggerEndpoint("./v1/swagger.json", "API for PDS"));
+            app.UseSwaggerUI(sw => { sw.SwaggerEndpoint("./v1/swagger.json", "API for PDS");
+                sw.RoutePrefix = string.Empty;
+            });
             //app.UseMvc();
            
    
